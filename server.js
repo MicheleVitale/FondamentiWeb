da3a9ee11872458811d2a635ad787ecd2c7a9762
Michele Vitale <m01.vitale@outlook.it>
2026-06-09 11:31:52 +0200

    Add files via upload
    Primo commit, creazione server e models job e user

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const cors = require ('cors');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+
+app.use(express.json());
+
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then (() => console.log('Connesso al DataBase'))
+.catch ((err) => console.log('Qualcosa è andato storto'))
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+    console.log("Server attivo sulla porta:" + PORT)
+})
+
